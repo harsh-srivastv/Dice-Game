15f68cee7543c99a2e68c4788dec02a1005380f4
Harsh Srivastava <harshsrivastv12@gmail.com>
2023-03-13 13:48:01 +0530

    refresh it updated

--- index.js
+++ index.js
@@ -16,12 +16,14 @@ var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 //If player 1 wins
-if (randomNumber1 > randomNumber2) {
+if (performance.getEntriesByType("navigation")[0].type === "reload") {
+  if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
-}
-else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-}
-else {
-  document.querySelector("h1").innerHTML = "Draw!";
+  }
+  else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  }
+  else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
 }
