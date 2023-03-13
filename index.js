635dc12228f417cea46e2f8d5aed03167e52763b
Harsh Srivastava <harshsrivastv12@gmail.com>
2023-03-13 13:33:38 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// Generate random number
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+
+// change image 1
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+// change image 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
